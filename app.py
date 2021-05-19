0d6808f920c4b3e8fb02a69ef980b3a3839f54ed
Sparsh Codes <singhsparsh@kccemsr.edu.in>
2021-05-20 00:33:13 +0530

    Bot is ready

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask, jsonify, request
+from flask import Flask, json, jsonify, request
 import time
 
 app = Flask(__name__)
 @app.route("/bot",methods= ['POST'])
 
 def response():
-    query = dict(request.form)['query']
-    # if query.lower() == "hi":
-    #     result = "Hello, how are you today?"
-    # elif query.lower() == "how are you":
-    #     result = "I'm good! How are you?"
-    # elif query.lower() == "what is your age?":
-    #     result = "I am just a session old xD"
-    # elif query.lower() == "bye":
-    #     result = "It was nice talking to you. Goodbye :)"
-    result = query + " " + time.ctime()
-    return jsonify({"response":result})
+    query = request.get_json()
+    # query = jsonquery["query"] + str(time.ctime())
+    if query["query"].lower() == "hi":
+        result = "Hello, how are you today?"
+    elif query["query"].lower() == "how are you":
+        result = "I'm good! How are you?"
+    elif query["query"].lower() == "what is your age?":
+        result = "I am just a session old xD"
+    elif query["query"].lower() == "bye":
+        result = "It was nice talking to you. Goodbye :)"
+    # result = query + " " + time.ctime()
+    return jsonify({"response": result})
 
 
 if __name__ == "__main__":
